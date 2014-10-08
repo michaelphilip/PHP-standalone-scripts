4c2f15e764187a9a9dbadd6297f79c1fc1c08424
Michael Favila <resourcemode@yahoo.com>
2014-10-08 14:00:11 +0800

    Update Crypt.php

--- Api/Crypt.php
+++ Api/Crypt.php
@@ -2,7 +2,7 @@ if (function_exists('openssl_encrypt')) {
     $textToEncrypt = "This is my information";
     $encryptionMethod = "AES-256-CBC";  // AES is used by the U.S. gov't to encrypt top secret documents.
     $secretKey = "25c6c7ff35b9979b151f2136cd13b0ff"; //32 chars
-	$iv = 'xyskdkdkdkdkdkd1'; //16 chars
+	$iv = 'aoe92js74ekke22s0'; //16 chars
 	
 	$delimiter = '|';
 	$ramdomSeedLen = 10;
@@ -15,7 +15,7 @@ if (function_exists('openssl_encrypt')) {
     $encryptedMessage = base64_encode(openssl_encrypt($text, $encryptionMethod, $secretKey, false, $iv));
     
     //To Decrypt
-	$encryptedMessage = base64_decode($encryptedMessage);
+    $encryptedMessage = base64_decode($encryptedMessage);
     $decryptedMessage = openssl_decrypt($encryptedMessage, $encryptionMethod, $secretKey, false, $iv);
     $decryptedMessage = substr($decryptedMessage, $omitLen, strlen($decryptedMessage) - (2 * $omitLen));
     //Result
