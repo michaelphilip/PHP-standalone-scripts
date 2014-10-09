593122177e9182f9c184e27435ea73a7a3690a32
Michael Favila <resourcemode@yahoo.com>
2014-10-09 18:46:05 +0800

    Update Crypt.php

--- Api/Crypt.php
+++ Api/Crypt.php
@@ -1,31 +1,38 @@
-if (function_exists('openssl_encrypt')) {
-    $textToEncrypt = "This is my information";
-    $encryptionMethod = "AES-256-CBC";  // AES is used by the U.S. gov't to encrypt top secret documents.
-    $secretKey = "25c6c7ff35b9979b151f2136cd13b0ff"; //32 chars
-	$iv = 'aoe92js74ekke22s0'; //16 chars
-	
-	$delimiter = '|';
-	$ramdomSeedLen = 10;
-	$omitLen = $ramdomSeedLen + strlen($delimiter);
-	
-	$seedA = generateRandomNumber(10);
-	$seedB = generateRandomNumber(10);
-	$text = $seedA . $delimiter . $textToEncrypt . $delimiter . $seedB;
-    //To encrypt
-    $encryptedMessage = base64_encode(openssl_encrypt($text, $encryptionMethod, $secretKey, false, $iv));
-    
-    //To Decrypt
-    $encryptedMessage = base64_decode($encryptedMessage);
-    $decryptedMessage = openssl_decrypt($encryptedMessage, $encryptionMethod, $secretKey, false, $iv);
-    $decryptedMessage = substr($decryptedMessage, $omitLen, strlen($decryptedMessage) - (2 * $omitLen));
-    
-    //Result
-    echo "Encrypted: $encryptedMessage <br/><br/> Decrypted: $decryptedMessage";
-}
-
-function generateRandomNumber($length)
+<?php
+class Crypt 
 {
-    $num = null;
-    for ($i = 0; $i < $length; $i++) $num .= mt_rand(0, 9);
-    return $num;
+    public function __construct()
+    {
+        if (function_exists('openssl_encrypt')) {
+            $textToEncrypt = "This is my information";
+            $encryptionMethod = "AES-256-CBC";  // AES is used by the U.S. gov't to encrypt top secret documents.
+            $secretKey = "25c6c7ff35b9979b151f2136cd13b0ff"; //32 chars
+        	$iv = 'aoe92js74ekke22s'; //16 chars
+        	
+        	$delimiter = '|';
+        	$ramdomSeedLen = 10;
+        	$omitLen = $ramdomSeedLen + strlen($delimiter);
+        	
+        	$seedA = self::generateRandomNumber(10);
+        	$seedB = self::generateRandomNumber(10);
+        	$text = $seedA . $delimiter . $textToEncrypt . $delimiter . $seedB;
+            //To encrypt
+            $encryptedMessage = base64_encode(openssl_encrypt($text, $encryptionMethod, $secretKey, false, $iv));
+            
+            //To Decrypt
+            $encryptedMessage = base64_decode($encryptedMessage);
+            $decryptedMessage = openssl_decrypt($encryptedMessage, $encryptionMethod, $secretKey, false, $iv);
+            $decryptedMessage = substr($decryptedMessage, $omitLen, strlen($decryptedMessage) - (2 * $omitLen));
+            
+            //Result
+            echo "Encrypted: $encryptedMessage <br/><br/> Decrypted: $decryptedMessage";
+        }
+    }
+
+    public static function generateRandomNumber($length)
+    {
+        $num = null;
+        for ($i = 0; $i < $length; $i++) $num .= mt_rand(0, 9);
+        return $num;
+    }
 }
